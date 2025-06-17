61246968ef89489189a6a7e4544d9027a1225332
Martin <igwem94@gmail.com>
2025-06-17 15:39:03 -0700

    first commit

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,29 @@
+import landing from "../assets/landing.png"
+import appdownload from "../assets/appDownload.png"
+
+const HomePage = ()=> {
+    return (
+        <div className="flex flex-col gap-12">
+            <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-13">
+                <h1 className="text-5xl font-bold tracking-tight text-black">
+                    Tuck into a takeway today
+                </h1>
+                <span className="text-xl">Food is just a click away</span>
+            </div>
+            <div className="grid md-grid-cols-2 gap-4">
+                <img src={landing}/>
+                <div className="flex flex-col items-center justify-center gap-4 text-center">
+                    <span className="font-bold text-3xl tracking-tighter">
+                        Order takeway even faster!
+                    </span>
+                    <span>
+                        Download LocalBukka App for faster ordering and personalised recommendations
+                    </span>
+                    <img src={appdownload}/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
